f8979d4bf81d990ec3beb501044af8fc255ec065
KhalidMh <khalid.marhoum97@gmail.com>
2024-09-25 23:35:37 +0100

    init package deps

---
+++ src/EloquentSQLServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace KhalidMh\EloquentSQL;
+
+use Illuminate\Support\ServiceProvider;
+
+class EloquentSQLServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Code to bootstrap your package's services
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Code to register your package's services
+    }
+}
